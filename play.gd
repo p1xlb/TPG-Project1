c0d3192f1ef44d86e7490bfad20d487bde0103e6
p1xlb <p1xlb@github.com>
2025-05-27 14:21:05 +0800

    hawk 8

--- play.gd
+++ play.gd
@@ -3,4 +3,4 @@ extends Button
 func _on_button_up() -> void:
 	$AnimationPlayer.play("transition end")
 	await $AnimationPlayer.animation_finished
-	get_tree().change_scene_to_file("res://Transition1.tscn")
+	get_tree().change_scene_to_file("res://splash_intro.tscn")
--- splash_intro.gd
+++ splash_intro.gd
@@ -2,4 +2,4 @@ extends Node
 
 func _ready() -> void:
 	await $VideoStreamPlayer.finished
-	get_tree().change_scene_to_file("res://startmenu.tscn")
+	get_tree().change_scene_to_file("res://Transition1.tscn")
